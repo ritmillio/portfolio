032bdd612193c3d82ad03664d4674f6f1dbc3ca2
Zoltan Fodor <me@zoltanfodor.dev>
2025-01-26 16:01:19 +0100

    chore: add a config file to store info

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const config = {
+    layout: {
+        meta: {
+            name: "FOLIO - Zoltan Fodor",
+            description: "A timeless, modern portfolio template built with Next.js, shadcn/ui, TypeScript and some magic :) to showcase your projects and skills with style."
+        },
+        lang: "en",
+        defaultTheme: "system" // light, dark, system
+    },
+    name: "Zoltan Fodor",
+    description: "Developer, Creator, Founder"
+
+}
