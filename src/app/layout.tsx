18c6c3f266966b395b2b341e67a078b10e1f18b7
Zoltan Fodor <me@zoltanfodor.dev>
2025-01-26 01:54:22 +0100

    feat: add next-theme + theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import "@/app/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+/** Providers */
 import { TRPCReactProvider } from "@/trpc/react";
+import { ThemeProvider } from "@/app/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -15,9 +17,22 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html
+      lang="en"
+      className={`${GeistSans.variable}`}
+      suppressHydrationWarning
+    >
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 
+import { ThemeToggle } from "@/components/common/theme-toggle";
+
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
@@ -10,8 +12,9 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <ThemeToggle />
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
