1efbbad3fef9784e9a6c8d6fe97a5e612306ee24
Zoltan Fodor <me@zoltanfodor.dev>
2025-01-26 16:07:00 +0100

    chore: update config

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,26 @@
 export const config = {
     layout: {
         meta: {
-            name: "FOLIO - Zoltan Fodor",
+            name: "folio - ritmillio",
             description: "A timeless, modern portfolio template built with Next.js, shadcn/ui, TypeScript and some magic :) to showcase your projects and skills with style."
         },
         lang: "en",
         defaultTheme: "system" // light, dark, system
     },
     name: "Zoltan Fodor",
-    description: "Developer, Creator, Founder"
-
+    description: "Developer, Creator, Founder",
+    links: {
+        github: "https://github.com/ritmillio",
+        linkedin: "https://www.linkedin.com/in/zoltan-fodor-007/",
+        x: "",
+        bluesky: "",
+        reddit: "",
+        instagram: "",
+        youtube: "",
+        threads: "",
+        tiktok: "",
+        spotify: "",
+        pinterest: "",
+        soundcloud: "",
+    }
 }
